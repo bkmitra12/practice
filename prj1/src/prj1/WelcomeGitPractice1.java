0e0a4272444a946c5c1b0184d651bffbe5a34fb0
Bhaba <mitra@talentech.com>
2019-11-18 16:53:26 -0500

    This is my 2nd file

---
+++ prj1/src/prj1/WelcomeGitPractice1.java
@@ -0,0 +1,11 @@
+package prj1;
+
+public class WelcomeGitPractice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my 2nd file");
+
+	}
+
+}
